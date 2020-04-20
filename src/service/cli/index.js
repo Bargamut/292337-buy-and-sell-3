68835079ffce74a8495b8d3020237f799f9abccd
Paul Petrov <paul@bargamut.ru>
2020-04-20 20:15:53 +0300

    Make commands export for src/service/cli/index.js

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,8 +4,12 @@ const help = require(`./help`);
 const version = require(`./version`);
 const generate = require(`./generate`);
 
+const cli = {
+  [generate.name]: generate,
+  [help.name]: help,
+  [version.name]: version,
+};
+
 module.exports = {
-  help,
-  version,
-  generate,
+  cli,
 };
