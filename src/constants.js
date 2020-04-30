39430314550655ccd6864b4cb327dc6b62ed2f13
Paul Petrov <paul@bargamut.ru>
2020-04-30 19:18:03 +0300

    Add HttpCode collection to src/constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,9 +6,17 @@ const ExitCode = {
   success: 0,
   error: 1
 };
+const HttpCode = {
+  OK: 200,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
 
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
   ExitCode,
+  HttpCode,
 };
