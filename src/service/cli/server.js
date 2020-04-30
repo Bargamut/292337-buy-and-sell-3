d1d13217e56c0092e3f40d2061720d33489ff3ef
Paul Petrov <paul@bargamut.ru>
2020-04-30 18:34:06 +0300

    Create src/service/cli/server.js

---
+++ src/service/cli/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const fs = require(`fs`);
+const http = require(`http`);
+const chalk = require(`chalk`);
+
+const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+    
+  },
+};
