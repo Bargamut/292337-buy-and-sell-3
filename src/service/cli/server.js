76b0f06191b5732b5115c6fea5a82fb7f0f09518
Paul Petrov <paul@bargamut.ru>
2020-04-30 18:45:09 +0300

    Add createServer method for server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -7,9 +7,28 @@ const chalk = require(`chalk`);
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
+const onClientConect = (req, res) => {};
+
+const createServer = (port) => {
+  http.createServer(onClientConect)
+    .listen(port)
+    .on(`listening`, (err) => {
+      if (err) {
+        return console.error(`Ошибка при создании сервера`, err);
+      }
+
+      return console.info(
+          chalk.green(`Ожидаю соединений на ${port}`)
+      );
+    });
+};
+
 module.exports = {
   name: `--server`,
   run(args) {
-    
+    const [customPort] = args;
+    const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    createServer(port);
   },
 };
