3c0863b95365396b2b81dad9498f96e041f9d4d3
Paul Petrov <paul@bargamut.ru>
2020-05-10 00:00:16 +0300

    Create template & route 4 render /search

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -7,6 +7,6 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.send(`/register`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
 
 module.exports = mainRouter;
