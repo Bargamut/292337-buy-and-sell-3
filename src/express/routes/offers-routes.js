ca3c73ce2d46e352905b8a60a302e572e0afc4db
Paul Petrov <paul@bargamut.ru>
2020-05-09 23:50:18 +0300

    Create template & route 4 render /offers/add

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 
 const offersRouter = new Router();
 
-offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
+offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
 offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
 offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
