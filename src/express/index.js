7f81f869367d4bc05a95ace0f6a165ce60a139ae
Paul Petrov <paul@bargamut.ru>
2020-05-09 22:05:00 +0300

    Create express/public/ dir & connect via middleware to index.js

--- src/express/index.js
+++ src/express/index.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
 const offersRouter = require(`./routes/offers-routes`);
 
+const PUBLIC_DIR = `public`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
+// middleware: Отдача статики с помощью
+app.use(
+    express.static(
+        path.resolve(__dirname, PUBLIC_DIR)
+    )
+);
+
+// Роутеры
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
 app.use(`/offers`, offersRouter);
