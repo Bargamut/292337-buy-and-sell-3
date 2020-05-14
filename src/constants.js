5a295e86f7b627b7f4a7bf579579f4d6394ba3bd
Paul Petrov <paul@bargamut.ru>
2020-05-14 14:31:17 +0300

    Add id length for mocks & move consts from generator

--- src/constants.js
+++ src/constants.js
@@ -13,10 +13,22 @@ const HttpCode = {
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
 };
+const MAX_ID_LENGTH = 6;
+const DEFAULT_COUNT = 1;
+const FILE_NAME = `mocks.json`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
   ExitCode,
   HttpCode,
+  MAX_ID_LENGTH,
+  DEFAULT_COUNT,
+  FILE_NAME,
+  FILE_TITLES_PATH,
+  FILE_SENTENCES_PATH,
+  FILE_CATEGORIES_PATH,
 };
