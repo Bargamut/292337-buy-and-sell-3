8512ea44194101ce6bb5b1a48c69428605d84266
Paul Petrov <paul@bargamut.ru>
2020-05-14 23:45:51 +0300

    Create offers api routes

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -4,10 +4,12 @@ const {Router} = require(`express`);
 
 const {getMockData} = require(`../lib/get-mock-data`);
 
+const offer = require(`./offer`);
 const category = require(`./category`);
 
 const {
-  CategoryService
+  CategoryService,
+  OfferService,
 } = require(`../data-service`);
 
 
@@ -17,6 +19,7 @@ const router = new Router();
   const mockData = await getMockData();
 
   category(router, new CategoryService(mockData));
+  offer(router, new OfferService(mockData) /* , new CommentService()*/);
 })();
 
 module.exports = router;
