6c83c94cc2f4b716c5efc5e04997e0dc1675afa0
Paul Petrov <paul@bargamut.ru>
2020-05-14 21:15:24 +0300

    BugFix: add forgotten return to reduce()

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -8,6 +8,7 @@ class CategoryService {
   findAll() {
     const categories = this._offers.reduce((acc, offer) => {
       acc.add(...offer.category);
+      return acc;
     }, new Set());
 
     return [...categories];
