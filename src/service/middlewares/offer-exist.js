29618d2af48e456ba41f4f2d953ae34453433f45
Paul Petrov <paul@bargamut.ru>
2020-05-14 22:45:37 +0300

    BugFix: add forgotten HTTP code to offer-exst middleware

--- src/service/middlewares/offer-exist.js
+++ src/service/middlewares/offer-exist.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const {HttpCode} = require(`../../constants`);
+
 module.exports = (service) => (req, res, next) => {
   const {offerId} = req.params;
   const offer = service.findOne(offerId);
 
   if (!offer) {
     return res
-      .status()
+      .status(HttpCode.NOT_FOUND)
       .send(`Offer with ${offerId} not found`);
   }
 
