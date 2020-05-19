ba799e93071ba21a96a763d02cafe962673333e8
Paul Petrov <paul@bargamut.ru>
2020-05-19 20:21:54 +0300

    Set destination for pino logs & update NPM scripts

--- src/service/lib/get-logger.js
+++ src/service/lib/get-logger.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const logger = require(`pino`)({
-  name: `api-service`,
-  level: process.env.LOG_LEVEL || `info`,
-});
+const pino = require(`pino`);
+const logger = pino(
+    {
+      name: `api-service`,
+      level: process.env.LOG_LEVEL || `info`,
+      prettyPrint: {
+        colorize: false,
+      },
+    },
+    pino.destination({
+      dest: `src/service/logs/all-logs`,
+      sync: false,
+    }),
+);
 
 module.exports = {
   logger,
