180f61da7a4cf206e545aa07d765f2d5909d7202
Paul Petrov <paul@bargamut.ru>
2020-05-22 21:26:10 +0300

    Remove comments data servoe 4 front server & refactor tpl /my/comments

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -7,17 +7,15 @@ const myRouter = require(`./my-routes`);
 const offersRouter = require(`./offers-routes`);
 
 const OfferDataService = require(`../data-service/offers-service`);
-const CommentDataService = require(`../data-service/comments-service`);
 
 const router = new Router();
 
 const url = `http://localhost:3000`;
 
 const offerDataService = new OfferDataService(url);
-const commentDataService = new CommentDataService(url);
 
 mainRouter(router, offerDataService);
 offersRouter(router, offerDataService);
-myRouter(router, offerDataService, commentDataService);
+myRouter(router, offerDataService);
 
 module.exports = router;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,20 +4,19 @@ const {Router} = require(`express`);
 
 const myRouter = new Router();
 
-module.exports = (parentRouter, offerDataService, commentDataService) => {
+module.exports = (parentRouter, offerDataService) => {
   parentRouter.use(`/my`, myRouter);
 
   myRouter.get(`/`, async (req, res) => {
     const offers = await offerDataService.findAll();
 
-    res.render(`my-tickets`, offers);
+    res.render(`my-tickets`, {offers});
   });
 
   myRouter.get(`/comments`, async (req, res) => {
     // Запросите комментарии к первым 3 публикациям с ресурса
     const offers = await offerDataService.findLast(3);
-    const comments = await commentDataService.findAll(offers);
 
-    res.render(`comments`, comments);
+    res.render(`comments`, {offers});
   });
 };
