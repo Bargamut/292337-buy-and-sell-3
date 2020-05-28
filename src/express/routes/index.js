3540932f4767fe17df6e734806d39af78492e078
Paul Petrov <paul@bargamut.ru>
2020-05-29 00:19:01 +0300

    Connect offer data service to offer router

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -8,6 +8,8 @@ const offersRouter = require(`./offers-routes`);
 
 const OfferDataService = require(`../data-service/offers-service`);
 
+const processDataMiddleware = require(`../middlewares/process-data-new-ticket`);
+
 const router = new Router();
 
 const url = `http://localhost:3000`;
@@ -15,7 +17,7 @@ const url = `http://localhost:3000`;
 const offerDataService = new OfferDataService(url);
 
 mainRouter(router, offerDataService);
-offersRouter(router, offerDataService);
+offersRouter(router, offerDataService, processDataMiddleware);
 myRouter(router, offerDataService);
 
 module.exports = router;
