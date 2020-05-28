d9617632f46a458a77d324395ec74e3b60cc47f8
Paul Petrov <paul@bargamut.ru>
2020-05-29 01:27:13 +0300

    Upgrade GET /search route: get search result & newest offers

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -25,5 +25,19 @@ module.exports = (parentRouter, offerDataService) => {
   });
   mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
   mainRouter.get(`/login`, (req, res) => res.render(`login`));
-  mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
+  mainRouter.get(`/search`, async (req, res) => {
+    const {search} = req.query;
+    const searchResult = await offerDataService.findBy(search);
+
+    // TODO: Убрать дублирование с newestOffers для главного маршрута
+    const offers = await offerDataService.findAll();
+
+    const newestOffers = [...offers].sort((a, b) => b.date - a.date);
+
+    if (newestOffers.length > 8) {
+      newestOffers.length = 8;
+    }
+
+    res.render(`search-result`, {searchResult, newestOffers});
+  });
 };
