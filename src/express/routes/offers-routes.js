b9ae53197e8a93db35849faac961ecbf38a503a9
Paul Petrov <paul@bargamut.ru>
2020-05-29 00:18:34 +0300

    Add POST route for /offers/add/

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const {Router} = require(`express`);
+const {
+  ALLOWED_FILE_TYPES,
+} = require(`../../constants`);
 
 const offersRouter = new Router();
 
-module.exports = (parentRouter, offerDataService) => {
+module.exports = (parentRouter, offerDataService, processDataMiddleware) => {
   parentRouter.use(`/offers`, offersRouter);
 
-  offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
+  offersRouter.get(`/add`, (req, res) => {
+    res.render(`new-ticket`, {
+      allowedFileTypes: ALLOWED_FILE_TYPES,
+    });
+  });
   offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
   offersRouter.get(`/edit/:id`, async (req, res) => {
     const {id: offerId} = req.params;
@@ -17,4 +24,19 @@ module.exports = (parentRouter, offerDataService) => {
     res.render(`ticket-edit`, {offer});
   });
   offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
+
+  offersRouter.post(`/add`, processDataMiddleware, async (req, res) => {
+    const {data: offer} = res.locals;
+
+    try {
+      await offerDataService.create(offer);
+
+      res.redirect(`/my`);
+    } catch (error) {
+      res.render(`new-ticket`, {
+        allowedFileTypes: ALLOWED_FILE_TYPES,
+        offer,
+      });
+    }
+  });
 };
